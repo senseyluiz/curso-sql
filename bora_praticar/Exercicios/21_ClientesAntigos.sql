ccbf16cfe65fae6cd4d5ea7e3131c81df04fdfe2
Luis Henrique <senseyluiz@gmail.com>
2026-01-27 15:33:24 -0300

    Exercicio 21

--- bora_praticar/Exercicios/21_ClientesAntigos.sql
+++ bora_praticar/Exercicios/21_ClientesAntigos.sql
@@ -1,2 +1,13 @@
-SELECT * 
-FROM Transacoes;
+SELECT C.IdCliente, 
+    C.DtCriacao AS DataCadastro,
+    COUNT(T.IdTransacao) AS TotalTransacoes
+FROM Transacoes as T
+
+LEFT JOIN clientes as C
+    ON T.IdCliente = C.IdCliente
+GROUP BY T.IdCliente, C.DtCriacao
+
+ORDER BY DataCadastro, TotalTransacoes DESC;
+
+
+
