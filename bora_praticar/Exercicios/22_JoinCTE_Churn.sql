488759244eeb2bd914e1fcdd8e27742b9192d0dc
Luis Henrique <senseyluiz@gmail.com>
2026-01-29 14:50:28 -0300

    Exercicio 22, sob re CTE

---
+++ bora_praticar/Exercicios/22_JoinCTE_Churn.sql
@@ -0,0 +1,19 @@
+WITH tb_cklientes_d1 as (
+    SELECT DISTINCT IdCliente
+    FROM transacoes
+    WHERE DATE(DtCriacao) = '2025-08-25'
+    
+)
+
+SELECT DATE(t2.DtCriacao) as dtDia,
+    COUNT(DISTINCT t1.IdCliente) as qtdCliente,
+    1. * COUNT(DISTINCT t1.IdCliente) / (SELECT COUNT(*) FROM tb_cklientes_d1) as txChurn
+FROM tb_cklientes_d1 as t1
+
+LEFT JOIN transacoes as t2
+    ON t1.IdCliente = t2.IdCliente
+    
+WHERE t2.DtCriacao >= '2025-08-25'
+AND t2.DtCriacao < '2025-08-30'
+
+GROUP BY dtDia;
